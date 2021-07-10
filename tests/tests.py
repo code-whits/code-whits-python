dbd84ab0e42e62f383a9f7d34e4b7d91ff3f4a25
Ajith Ramachandran <ajithar204@gmail.com>
2021-07-10 15:24:49 +0530

    change json structure

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from lib import factorial
+from lib import factorial, sum
 
 class CWTestCase(unittest.TestCase):
 
@@ -8,6 +8,11 @@ class CWTestCase(unittest.TestCase):
         self.assertEqual(factorial(5), 120)
         self.assertEqual(factorial(10), 3628800)
         self.assertEqual(factorial(1), 1)
+    
+    def test_sum(self):
+        self.assertEqual(sum(5), 15)
+        self.assertEqual(sum(10), 55)
+        self.assertEqual(sum(1), 1)
 
 if __name__ == '__main__':
     unittest.main()
