77aecf2ab8a143a94d8749b3a5c678a7e97985b7
Ajith Ramachandran <ajithar204@gmail.com>
2021-07-13 17:08:41 +0530

    add hex to rgb function

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,2 +1,3 @@
 from .factorial import factorial
 from .sum import sum
+from .hex_to_rgb import hex_to_rgb
--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from lib import factorial, sum
+from lib import factorial, sum, hex_to_rgb
 
 class CWTestCase(unittest.TestCase):
 
@@ -13,6 +13,11 @@ class CWTestCase(unittest.TestCase):
         self.assertEqual(sum(5), 15)
         self.assertEqual(sum(10), 55)
         self.assertEqual(sum(1), 1)
+    
+    def test_hex_to_rgb(self):
+        self.assertEqual(hex_to_rgb('2d2d2d'), (45, 45, 45))
+        self.assertEqual(hex_to_rgb('011926'), (1, 25, 38))
+        self.assertEqual(hex_to_rgb('ffb80b'), (255, 184, 11))
 
 if __name__ == '__main__':
     unittest.main()
